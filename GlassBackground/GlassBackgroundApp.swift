a6139a4744751dd272d2e8e88d471ee0493aa8b1
eyhdev <alaqsa335@gmail.com>
2023-09-20 15:52:29 +0300

    Initial Commit

---
+++ GlassBackground/GlassBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassBackgroundApp.swift
+//  GlassBackground
+//
+//  Created by eyh.mac on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GlassBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
